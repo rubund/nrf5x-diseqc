2be2ef420dd2ff0ca38f1e371c3b1490ae5d907d
Ruben Undheim <ruben.undheim@nordicsemi.no>
2018-05-29 11:15:28 +0200

    Toggling pin

--- src/dut.c
+++ src/dut.c
@@ -1,9 +1,19 @@
+#include <nrf.h>
+#include <nrf_delay.h>
 
 
 
 int main()
 {
 
+    NRF_P0->PIN_CNF[27] = 0x00000003;
 
+
+    while(1) {
+        nrf_delay_us(20);
+        NRF_P0->OUTSET = (1 << 27);
+        nrf_delay_ms(1);
+        NRF_P0->OUTCLR = (1 << 27);
+    }
 }
 
