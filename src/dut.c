57d3cdb97e7d95257cd29e6544afcf0cd56b6ef3
Ruben Undheim <ruben.undheim@nordicsemi.no>
2018-05-29 12:39:57 +0200

    Turning on and off switching

--- src/dut.c
+++ src/dut.c
@@ -25,27 +25,34 @@ int main()
   //
   //  NRF_PWM0->TASKS_SEQSTART[0] = 1;
 
-    uint16_t pwm_seq[4] = {50,15,15,20};
+    uint16_t pwm_seq[8] = {50,30,10,100,100,50,40,100};
     NRF_PWM0->PSEL.OUT[0] = (27 << PWM_PSEL_OUT_PIN_Pos) |
                             (PWM_PSEL_OUT_CONNECT_Connected <<
                                                      PWM_PSEL_OUT_CONNECT_Pos);
-    NRF_PWM0->PSEL.OUT[1] = (28 << PWM_PSEL_OUT_PIN_Pos) |
-                            (PWM_PSEL_OUT_CONNECT_Connected <<
-                                                     PWM_PSEL_OUT_CONNECT_Pos);
+    //NRF_PWM0->PSEL.OUT[1] = (28 << PWM_PSEL_OUT_PIN_Pos) |
+    //                        (PWM_PSEL_OUT_CONNECT_Connected <<
+    //                                                 PWM_PSEL_OUT_CONNECT_Pos);
     NRF_PWM0->ENABLE      = (PWM_ENABLE_ENABLE_Enabled << PWM_ENABLE_ENABLE_Pos);
     NRF_PWM0->MODE        = (PWM_MODE_UPDOWN_Up << PWM_MODE_UPDOWN_Pos);
     NRF_PWM0->PRESCALER   = (PWM_PRESCALER_PRESCALER_DIV_2 <<
                                                      PWM_PRESCALER_PRESCALER_Pos);
-    NRF_PWM0->COUNTERTOP  = (100 << PWM_COUNTERTOP_COUNTERTOP_Pos); //1 msec
+    //NRF_PWM0->COUNTERTOP  = (100 << PWM_COUNTERTOP_COUNTERTOP_Pos); //1 msec
     NRF_PWM0->LOOP        = (PWM_LOOP_CNT_Disabled << PWM_LOOP_CNT_Pos);
-    NRF_PWM0->DECODER   = (PWM_DECODER_LOAD_Individual << PWM_DECODER_LOAD_Pos) |
-                          (PWM_DECODER_MODE_RefreshCount << PWM_DECODER_MODE_Pos);
+    NRF_PWM0->DECODER   = (PWM_DECODER_LOAD_WaveForm << PWM_DECODER_LOAD_Pos) |
+                          (PWM_DECODER_MODE_NextStep << PWM_DECODER_MODE_Pos);
     NRF_PWM0->SEQ[0].PTR  = ((uint32_t)(pwm_seq) << PWM_SEQ_PTR_PTR_Pos);
     NRF_PWM0->SEQ[0].CNT  = ((sizeof(pwm_seq) / sizeof(uint16_t)) <<
                                                      PWM_SEQ_CNT_CNT_Pos);
     NRF_PWM0->SEQ[0].REFRESH  = 0;
     NRF_PWM0->SEQ[0].ENDDELAY = 0;
-    NRF_PWM0->TASKS_SEQSTART[0] = 1;
+    while(1) {
+        NRF_PWM0->TASKS_SEQSTART[0] = 1;
+        nrf_delay_ms(10);
+        NRF_PWM0->TASKS_NEXTSTEP = 1;
+        nrf_delay_ms(5);
+        NRF_PWM0->TASKS_STOP = 1;
+        nrf_delay_ms(5);
+    }
 
 }
 
